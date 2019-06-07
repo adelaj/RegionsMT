48e438702e036d34ee2ede97f799bca7726c408b
root <root@DESKTOP-CORE.localdomain>
2019-06-08 01:36:44 +0300

    ...

--- .install/install.ps1
+++ .install/install.ps1
@@ -19,7 +19,7 @@ foreach ($i in $lib)
         if (-Not (Test-Path $i-$j)) { New-Item -ItemType Directory -Force -Path $i-$j }
         cd $i-$j
         ${lib-flags} = (Get-Variable -Name ("cmake-flags-" + $i)).Value.Trim()
-        cmake $(if (${lib-flags}) { ${lib-flags} + " " })-D MSVC="ON" -D CMAKE_C_FLAGS_INIT="/GL" -D CMAKE_STATIC_LINKER_FLAGS_INIT="/LTCG" -D CMAKE_GENERATOR_PLATFORM="$j" $(join-path .. $i)
+        cmake $(if (${lib-flags}) { ${lib-flags} + " " })-D CMAKE_C_FLAGS_INIT="/GL" -D CMAKE_STATIC_LINKER_FLAGS_INIT="/LTCG" -D CMAKE_GENERATOR_PLATFORM="$j" $(join-path .. $i)
         foreach ($k in $CFG.Split(" ")) 
         {     
             # & $msbuild "$i.sln" /t:$i /p:Configuration=$k /p:CharacterSet=Unicode 
