945a554e8d0fc927dce37d1a2a747a8f542de41d
root <root@DESKTOP-CORE.localdomain>
2019-06-10 03:24:58 +0300

    ...

--- .install/pthread-win32/prologue.ps1
+++ .install/pthread-win32/prologue.ps1
@@ -3,4 +3,6 @@ $lib = "pthread-win32"
 if ($CLR -And (Test-Path $lib)) { rm $lib -Force -Recurse }
 if (-Not (Test-Path $lib)) { git clone "https://github.com/GerHobbelt/pthread-win32" $lib }
 $cmakelists = Join-Path $lib "CMakeLists.txt"
-if (-Not (Test-Path $cmakelists)) { Copy-Item (Join-Path (Split-Path -parent $PSCommandPath) "CMakeLists.txt") $cmakelists }
+#if (-Not (Test-Path $cmakelists)) { 
+Copy-Item (Join-Path (Split-Path -parent $PSCommandPath) "CMakeLists.txt") $cmakelists
+# }
