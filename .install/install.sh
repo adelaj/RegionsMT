7ef786a2e3d42f3118129ec57fdf44bfb01af622
root <root@DESKTOP-CORE.localdomain>
2019-06-15 18:31:15 +0300

    ...

--- .install/install.sh
+++ .install/install.sh
@@ -12,20 +12,23 @@ C_FLAGS_i386="-m32"
 C_FLAGS_x86_64="-m64"
 build_lib() {
     C_FLAGS_INIT=$(echo $C_FLAGS $(eval echo "\$C_FLAGS_$2") | xargs)
-    cmake -D CMAKE_BUILD_TYPE="$3" -D CMAKE_C_FLAGS_INIT="$C_FLAGS_INIT" -D CMAKE_AR="/usr/bin/gcc-ar" -D CMAKE_C_ARCHIVE_CREATE="<CMAKE_AR> qcs <TARGET> <OBJECTS>" -D CMAKE_C_ARCHIVE_FINISH=true -D CMAKE_REQUIRED_LIBRARIES="m" "../$SRC/$1"
+    cmake -D CMAKE_BUILD_TYPE="$3" -D CMAKE_C_FLAGS_INIT="$C_FLAGS_INIT" -D CMAKE_AR="/usr/bin/gcc-ar" -D CMAKE_C_ARCHIVE_CREATE="<CMAKE_AR> qcs <TARGET> <OBJECTS>" -D CMAKE_C_ARCHIVE_FINISH=true -D CMAKE_REQUIRED_LIBRARIES="m" "../../$SRC/$1"
     cmake --build . --target $1 -- VERBOSE=1
 }
 for i in $LIB; do
     SDIR=$(eval dirname $0)/$i
     if [ -f "$SDIR/prologue.sh" ]; then SRC=$SRC CLR=$CLR bash $SDIR/prologue.sh; fi 
     for j in $ARCH; do
+        DIR=$i-$j
+        mkdir -p $DIR
+        cd $DIR
         for k in $CFG; do
-            DIR=$i-$j-$k
-            mkdir -p $DIR
-            cd $DIR
+            mkdir -p $k
+            cd $k
             build_lib $i $j $k
             cd ..
         done
+        cd ..
     done
     if [ -f "$SDIR/epilogue.sh" ]; then SRC=$SRC CLR=$CLR bash $SDIR/epilogue.sh; fi
 done
