1743b4bf9f77575246b5de549177022e25517e53
Adelaj <kersenel@gmail.com>
2019-07-13 23:53:11 +0300

    ...

--- .install/install_libraries.sh
+++ .install/install_libraries.sh
@@ -3,6 +3,7 @@
 if [ -z "$ARCH" ]; then ARCH=$(arch); fi
 if [ -z "$CFG" ]; then CFG=Release; fi
 if [ -z "$BUILD_PATH" ]; then BUILD_PATH="."; fi
+if [ -z "$LOG_PATH" ]; then LOG_PATH=$BUILD_PATH/.`basename $0 .${0##*.}`_`date +%s`; fi
 if [ -z "$TOOLCHAIN" ]; then TOOLCHAIN="gcc"; fi
 if [ -z "$LIBRARY" ]; then LIBRARY="gsl"; fi
 
@@ -15,57 +16,68 @@ C_FLAGS=( \
 [,i386,]="-m32" \
 [,x86_64,]="-m64" \
 [,,Release]="-flto" \
-[,,Debug]="-g" \
 [gcc,,Release]="-fuse-linker-plugin" \
 [gcc,,Debug]="-Og" \
 [clang,,Debug]="-O0")
 
-declare -A dir pid 
-declare -A BUILD_CC BUILD_CXX BUILD_AR BUILD_C_FLAGS
+wait_pid() {
+    declare -i res=0 &&
+    for p in $1; do 
+        wait $p || let "res=1"
+    done &&
+    return $res
+}
 
 build_vars_2() {
     for v in $1; do
         declare -g -A BUILD_$v[$2,$3,]="$(echo $(eval echo \
         \${$v'[,,]'} \${$v'[$2,,]'} \${$v'[,$3,]'} \${$v'[$2,$3,]'}) | xargs)"
-        echo BUILD_$v[$2,$3,] = $(eval echo \${BUILD_$v'['$t','$i',]'})
+        # echo BUILD_$v[$2,$3,] = $(eval echo \${BUILD_$v'[$t,$i,]'})
     done
 }
 
 build_vars_3() {
     for v in $1; do
-        declare BUILD_$v[$2,$3,$4]="$(echo $(eval echo \
-        \${BUILD_$v[$2,$3,]} \${$v[,,$4]} \${$v[$2,,$4]} \${$v[,$3,$4]} \${$v[$2,$3,$4]}) | xargs)"
+        declare -g -A BUILD_$v[$2,$3,$4]="$(echo $(eval echo \
+        \${BUILD_$v'[$2,$3,]'} \${$v'[,,$4]'} \${$v'[$2,,$4]'} \${$v'[,$3,$4]'} \${$v'[$2,$3,$4]'}) | xargs)"
+        # echo BUILD_$v[$2,$3,$4] = $(eval echo \${BUILD_$v'[$2,$3,$4]'})
     done
 }
 
 toolset_vars() {
     for v in $1; do
         if [ -z "$(eval echo \${$v[$2]})" ]; then 
-            declare BUILD_$v[$2]="$(eval echo \${$v[$3]}$4)"
+            declare -g -A BUILD_$v[$2]="$(eval echo \${$v'[$3]'}$4)"
         else
-            declare BUILD_$v[$2]="$(eval echo \${$v[$2]})"
+            declare -g -A BUILD_$v[$2]="$(eval echo \${$v'[$2]'})"
         fi
-        echo BUILD_$v[$2] = $(eval echo \${BUILD_$v["$2"]})
-        echo BUILD_$v : $(eval echo \${!BUILD_$v[@]})
     done
 }
 
+build_library_cmake_cfg() {
+    mkdir -p "$LOG_PATH/$2/$1-$3" &&
+    cmake \
+    -B "$2/$1-$3/$4" \
+    -D CMAKE_C_COMPILER="${BUILD_CC[$2]}" \
+    -D CMAKE_CXX_COMPILER="${BUILD_CXX[$2]}" \
+    -D CMAKE_AR="${BUILD_AR[$2]}" \
+    -D CMAKE_BUILD_TYPE="$4" \
+    -D CMAKE_C_FLAGS_INIT="${BUILD_C_FLAGS[$2,$3,$4]}" \
+    -D CMAKE_C_ARCHIVE_CREATE="<CMAKE_AR> qcs <TARGET> <OBJECTS>" \
+    -D CMAKE_C_ARCHIVE_FINISH="" \
+    "$1" \
+    >"$LOG_PATH/$2/$1-$3/$4.log" 2>&1 &&
+    cmake --build "$2/$1-$3/$4" --target $1 -- VERBOSE=1 -j \
+    >>"$LOG_PATH/$2/$1-$3/$4.log" 2>&1
+}
+
 build_library_cmake() {
+    declare pid &&
     for k in $CFG; do
-        mkdir -p "$2/$1-$3/$k"
-        echo BUILD_CC[$2] = $(eval echo \${BUILD_CC[$2]})
-        cmake \
-        -B "$2/$1-$3/$k" \
-        -D CMAKE_C_COMPILER="${BUILD_CC[$2]}" \
-        -D CMAKE_CXX_COMPILER="${BUILD_CXX[$2]}" \
-        -D CMAKE_AR="${BUILD_AR[$2]}" \
-        -D CMAKE_BUILD_TYPE="$k" \
-        -D CMAKE_C_FLAGS_INIT="${BUILD_C_FLAGS[$2,$3,$k]}" \
-        -D CMAKE_C_ARCHIVE_CREATE="<CMAKE_AR> qcs <TARGET> <OBJECTS>" \
-        -D CMAKE_C_ARCHIVE_FINISH="ON" \
-        "$1"
-        cmake --build "$2/$1-$3/$k" --target $1 -- VERBOSE=1 -j
-    done
+        build_library_cmake_cfg $1 $2 $3 $k &
+        pid="$pid $!"
+    done &&
+    wait_pid "$pid"
 }
 
 build_library_xcode() {
@@ -75,18 +87,25 @@ build_library_xcode() {
     -G "Xcode" \
     -D CMAKE_OSX_ARCHITECTURES="$2" \
     -D CMAKE_C_FLAGS_INIT="$(eval echo \${BUILD_C_FLAGS[Xcode,$2,$3]})" \
-    "$1"
+    "$1" &&
+    declare pid &&
     for i in $CFG; do
-        cmake --build "Xcode/$1-$2" --target $1 --config $i -- -j
-    done
+        cmake --build "Xcode/$1-$2" --target $1 --config $i -- -j &
+        pid="$pid $!"
+    done &&
+    wait_pid "$pid"
 }
 
 download_library_git() {
+    mkdir -p "$LOG_PATH" &&
     if [ -d "$1" ]; then
-        git -C "$1" reset --hard HEAD
-        git -C "$1" pull        
+        git -C "$1" reset --hard HEAD \
+        >"$LOG_PATH/$1.log" 2>&1 &&
+        git -C "$1" pull \
+        >>"$LOG_PATH/$1.log" 2>&1    
     else
-        git clone --depth 1 "$2" "$1"                
+        git clone --depth 1 "$2" "$1" \
+        >"$LOG_PATH/$1.log" 2>&1
     fi
 }
 
@@ -102,28 +121,36 @@ build_gsl() {
     fi
 }
 
+install_library() {
+    download_$1 &&
+    declare pid &&
+    for t in $TOOLCHAIN; do
+        for i in $ARCH; do
+            build_$1 $t $i &
+            pid="$pid $!"
+        done
+    done &&
+    wait_pid "$pid"
+}
+
 install_libraries() {
     for t in $TOOLCHAIN; do
         if [ "$t" != "Xcode" ]; then
             toolset_vars "CC CXX AR" $t ${t%%-*} ${t#${t%%-*}*}
-            echo BUILD_CC : $(eval echo \${!BUILD_CC[@]})
         fi
         for i in $ARCH; do
             build_vars_2 "C_FLAGS" $t $i
-            echo BUILD_C_FLAGS[$t,$i,] = ${BUILD_C_FLAGS[$t,$i,]}
             for j in $CFG; do 
                 build_vars_3 "C_FLAGS" $t $i $j
-                echo BUILD_C_FLAGS[$t,$i,$j] = ${BUILD_C_FLAGS[$t,$i,$j]}
-            done
-        done
-        for i in $LIBRARY; do
-            download_$i
-            for j in $ARCH; do
-                build_$i $t $j
             done
         done
     done
-    cd "$SOURCE_PATH"
+    declare pid &&
+    for i in $LIBRARY; do
+        install_library $i &
+        pid="$pid $!"
+    done &&
+    wait_pid "$pid"
 }
 
 uninstall_libraries() {
